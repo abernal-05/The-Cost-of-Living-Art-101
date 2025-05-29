07b084a7bdbf39ceb2100d05a66b67da44bcf293
abernal-05 <arantxabernal71@gmail.com>
2025-05-28 20:09:16 -0700

    transportation-page

---
+++ transportation/js/counter.js
@@ -0,0 +1,24 @@
+// Transportation JavaScript
+
+$(document).ready(function(){
+
+    console.log("js & doc loaded");
+
+    $("#bus").click(function(){
+        console.log("User chose the bus.")
+    });
+    
+
+    $("#car").click(function() {
+
+        console.log("User chose to drive.");
+    });
+
+    $("#bike").click(function() {
+
+        console.log("User chose to bike.");
+    });
+
+});
+
+
