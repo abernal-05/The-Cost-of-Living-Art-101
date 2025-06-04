e2d069598dbc3699256544735a9b1aad085b30fe
abernal-05 <arantxabernal71@gmail.com>
2025-06-04 16:22:03 -0700

    transportation

--- day1/transportation/js/counter.js
+++ day1/transportation/js/counter.js
@@ -6,17 +6,34 @@ $(document).ready(function(){
 
     $("#bus").click(function(){
         console.log("User chose the bus.")
+
+        str = "The bus is free, and will get you to class in 25 minutes"
+
+        $("#chosen-transport").append('<p>' + str + '</p>')
+        
+        
+
     });
     
 
-    $("#car").click(function() {
-
+    $("#drive").click(function() {
         console.log("User chose to drive.");
+        
+        str = "Your tank is almost empty, filling it will cost $60... The nearest station is 5 minutes away, and you're a 10 minute drive from campus."
+
+        $("#chosen-transport").append('<p>' + str + '</p>')
+        
+        
     });
 
     $("#bike").click(function() {
-
         console.log("User chose to bike.");
+
+        str = "The biking is free, and will get you to class in 30 minutes."
+
+        $("#chosen-transport").append('<p>' + str + '</p>')
+        
+        
     });
 
 });
