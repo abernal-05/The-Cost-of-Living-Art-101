761c11786e5b5ba65a87f202b89fd9bc73234fb6
Tsukiiyakii <alphinathewolf22@gmail.com>
2025-06-10 02:58:13 -0700

    tofu

--- day1/page4/java/java.js
+++ day1/page4/java/java.js
@@ -2,8 +2,13 @@ function toggleOatInCart() {
     $("#oat-in-cart").toggle();
     $("#oatmilk-button-img").hide();
   }
-  
+
   function toggleWholeInCart() {
     $("#whole-in-cart").toggle();
     $("#wholemilk-button-img").hide();
+  }
+
+   function toggleTofuInCart() {
+    $("#tofu-in-cart").toggle();
+    $("#tofuburger-button-img").hide();
   }
