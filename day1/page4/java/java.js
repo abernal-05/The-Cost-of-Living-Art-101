a24a741d4cbd1eae538e946339a7c369aafde203
audrey-de-merch <ademerch@ucsc.edu>
2025-06-10 03:18:36 -0700

    choice

--- day1/page4/java/java.js
+++ day1/page4/java/java.js
@@ -1,18 +1,22 @@
 function toggleOatInCart() {
     $("#oat-in-cart").toggle();
     $("#oatmilk-button-img").hide();
+    $("#wholemilk-button-img").hide();
   }
 
   function toggleWholeInCart() {
     $("#whole-in-cart").toggle();
     $("#wholemilk-button-img").hide();
+    $("#oatmilk-button-img").hide();
   }
 
    function toggleTofuInCart() {
     $("#tofu-in-cart").toggle();
     $("#tofuburger-button-img").hide();
+    $("#beefburger-button-img").hide();
   }
     function toggleBeefInCart() {
     $("#beef-in-cart").toggle();
     $("#beefburger-button-img").hide();
+    $("#tofuburger-button-img").hide();
   }
