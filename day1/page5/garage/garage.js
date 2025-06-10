c4d2e93c69d0b91662d000be930317d190c4f7d4
abernal-05 <arantxabernal71@gmail.com>
2025-06-10 01:39:06 -0700

    fix

--- day1/page5/garage/garage.js
+++ day1/page5/garage/garage.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     $("#bike-img").click(function(){
         console.log("Going to class.")
-        window.location.href = "../../page6/index.html";
+        window.location.href = '../../page6/index.html';
     });
 
 })
